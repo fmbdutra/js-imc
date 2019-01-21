82c097d4b7dae8a865a783062e6874351443439d
Fabiano Dutra <fmbdutra@gmail.com>
2019-01-21 11:03:52 -0200

    Animando a remoção do paciente aula 7 part 6

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,27 +1,12 @@
 var pacientes = document.querySelectorAll(".paciente");
-
-/*
-pacientes.forEach(function(paciente){
-	paciente.addEventListener("dblclick", function(){
-		console.log("Fui clicado com um duplo clique");
-		this.remove();
-	});
-});
-//console.log(pacientes);
-
-*/
-
 var tabela = document.querySelector("table");
 
-tabela.addEventListener("dblclick", function(){
-	/*
-	var alvoEvento = event.target;
-	var paiDoAlvo = alvoEvento.parentNode; // TR = paciente = remover
+tabela.addEventListener("dblclick", function(event){
+	event.target.parentNode.classList.add(" fadeOut");
 
-	paiDoAlvo.remove();
-	*/
-
-	event.target.parentNode.remove();
+	setTimeout(function(){
+		event.target.parentNode.remove();
+	}, 500);
 })
 
 
