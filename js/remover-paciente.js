87683619aae5eab87066967eee61a7de607e23ee
Fabiano Dutra <fmbdutra@gmail.com>
2019-01-22 11:28:31 -0200

    ajustado espaços aula 7 fim

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -2,7 +2,7 @@ var pacientes = document.querySelectorAll(".paciente");
 var tabela = document.querySelector("table");
 
 tabela.addEventListener("dblclick", function(event){
-	event.target.parentNode.classList.add(" fadeOut");
+	event.target.parentNode.classList.add("fadeOut");
 
 	setTimeout(function(){
 		event.target.parentNode.remove();
