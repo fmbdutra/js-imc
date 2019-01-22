db244001b7cc74887e67e5bfc3eb2cd2d2c4b3ec
Fabiano Dutra <fmbdutra@gmail.com>
2019-01-22 16:22:44 -0200

    add adiciona mensagem erro - aula 9-5

--- js/buscar-pacientes.js
+++ js/buscar-pacientes.js
@@ -4,24 +4,30 @@ botaoAdicionar.addEventListener("click", function(){
 	console.log("Buscando pacientes");
 
 	var xhr= new XMLHttpRequest();
+	
 	xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
-	
+
+	//xhr.open("GET", "https://api-pacientes.herokuapp.com/pa111cientes");
+	//Simula erro 
+
 	xhr.addEventListener("load", function(){
-		var resposta = xhr.responseText;
-		console.log(resposta);
-		console.log(typeof resposta);
 
-		var pacientes = JSON.parse(resposta);
-		console.log(pacientes);
-		console.log(typeof pacientes);
+	    var erroAjax = document.querySelector("#erro-ajax");
+	    if (xhr.status == 200) {
+	        erroAjax.classList.add("invisivel");
+	        var resposta = xhr.responseText;
+	        var pacientes = JSON.parse(resposta);
 
-		pacientes.forEach(function(paciente){
-			adicionaPacienteNaTabela(paciente);
-		})
-		
-	})
-	
+	        pacientes.forEach(function(paciente) {
+	            adicionaPacienteNaTabela(paciente);
+	        });
+	    } else {
+	        erroAjax.classList.remove("invisivel");
+	    }
+	});
+
 	xhr.send();
+
 });
 
 
